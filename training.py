fff19b1c294281ea369a12242aabd5f124e25095
Chandrakant Ojha <chandrkant.o14@iiits.in>
2017-03-24 23:02:05 +0530

    update

--- training.py
+++ training.py
@@ -1,12 +1,17 @@
+# Extracting features using optical flow
+# walking:1, Jogging:2, Handclapping: 3, Handwaving: 4, running: 5, boxing: 6 	
+#---------------------------------------------------------------------------------
+
 import numpy as np
 import cv2
 import imutils
 from sklearn.cluster import KMeans
 import math
+import os
 
-cap = cv2.VideoCapture("videos/person15_running_d1_uncomp.avi")
-
-
+# cap = cv2.VideoCapture("videos/person15_running_d1_uncomp.avi")
+labels = {'walking':1, 'Jogging':2, 'Handclapping': 3, 'Handwaving': 4, 'running': 5, 'boxing': 6 }
+global Frames
 	# params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
 	                   qualityLevel = 0.3,
@@ -18,30 +23,31 @@ lk_params = dict( winSize  = (15,15),
 	              maxLevel = 2,
 	              criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-def ExtractFeatures():
+def ExtractFeatures(cap):
 	# Create some random colors
 	color = np.random.randint(0,255,(100,3))
 
 	# Take first frame and find corners in it
 	ret, old_frame = cap.read()
+	# old_frame = imutils.resize(old_frame, width=128, height=128)
+	old_frame = cv2.resize(old_frame, (128,128), interpolation = cv2.INTER_AREA)
 	old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 	p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-
-	w, h = len(old_frame), len(old_frame[0])
-	Frames = np.empty([0, w*h], dtype="uint8")
-
+	
+	# w, h = len(old_frame), len(old_frame[0])
+	Frames = np.empty([0, 128*128], dtype="uint8")  # Frames -> will store the features in which each row will represent the one frame of the video. 
 
 	# Create a maxCornersk image for drawing purposes
 	mask = np.zeros_like(old_frame)
-	print("mask.shape",mask.shape)
+	# print("mask.shape",mask.shape)
 	count = 0
 	while True:
 	    ret,frame = cap.read()
 	    count+=1
-	    
 	    if not ret:
 	    	break
 
+	    frame = cv2.resize(frame, (128,128), interpolation = cv2.INTER_AREA)
 	    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	    # calculate optical flow
@@ -65,6 +71,7 @@ def ExtractFeatures():
 	    img = imutils.resize(img, width=500)
 	    # mask = imutils.resize(mask, width=500)
 	    
+	    # print ("len(mask):", len(mask))
 	    m = np.around(mask[:,:,1].ravel(), decimals=3)
 
 	    Frames = np.vstack((Frames, m))
@@ -73,7 +80,7 @@ def ExtractFeatures():
 	    cv2.imshow('mask', mask[:,:,1])
 	    k = cv2.waitKey(30) & 0xff
 	    if k == 27:
-	        breakr
+	        break
 	    
 	    # Now update the previous frame and previous points
 	    old_gray = frame_gray.copy()
@@ -84,45 +91,69 @@ def ExtractFeatures():
 
 	cv2.destroyAllWindows()
 	cap.release()
-	return Frames
+	return count
 
 
-def Clustering(Frames):
+# def Clustering(Frames):
 	
-	iter_=2
-	prev_var=0
-	while iter_ < len(Frames)/2:
-		kmeans = KMeans(n_clusters=iter_, random_state=0).fit(Frames)
-		new_var = 0
-		for j in range(iter_):
-			indices = [i for i, x in enumerate(kmeans.labels_) if x == j]
-			li=list()
-			for k in range(len(indices)):
-				li.append(Frames[k])
-			li = np.asarray(li)
-			new_var+=np.var(li)
+# 	iter_=2
+# 	prev_var=0
+# 	while iter_ < len(Frames)/2:
+# 		kmeans = KMeans(n_clusters=iter_, random_state=0).fit(Frames)
+# 		new_var = 0
+# 		for j in range(iter_):
+# 			indices = [i for i, x in enumerate(kmeans.labels_) if x == j]
+# 			li=list()
+# 			for k in range(len(indices)):
+# 				li.append(Frames[k])
+# 			li = np.asarray(li)
+# 			new_var+=np.var(li)
 		
-		new_var = math.sqrt(new_var)
-		print(iter_)
-		print("new_var:", new_var)
+# 		new_var = math.sqrt(new_var)
+# 		print(iter_)
+# 		print("new_var:", new_var)
 
-		if iter_==2:
-			prev_var = new_var
-		elif new_var < prev_var:
-			prev_var = new_var
-		else:
-			break
+# 		if iter_==2:
+# 			prev_var = new_var
+# 		elif new_var < prev_var:
+# 			prev_var = new_var
+# 		else:
+# 			break
 			
-		iter_+=1
-		prev_kmeansLabels = kmeans.labels_
+# 		iter_+=1
+# 		prev_kmeansLabels = kmeans.labels_
 
-	return iter_, prev_kmeansLabels
+# 	return iter_, prev_kmeansLabels
 
 def main():
-	Feature = ExtractFeatures()
-	n, labels = Clustering(Feature)
-	print("no. of lables:", n)
-	print("*", labels)
+	# labels = list()
+	global labels
+	labels = {'walking':1, 'jogging':2, 'handclapping': 3, 'handwaving': 4, 'running': 5, 'boxing': 6 }
+	i=0
+	j=0
+	filename = []
+	Feature_labels = list()
+	directory = os.path.join("c:\\","/home/chandu/Activity Anticipation/videos")
+	for root,dirs,files in os.walk(directory):
+		for file in files:
+			filename.append(file)
+
+	for it in range(len(filename)):
+		temp = list()
+		cap = cv2.VideoCapture("videos/"+filename[it])
+		count = ExtractFeatures(cap)
+		print ("###", type(labels))
+		name = filename[it].split('.')
+		temp.append(int(labels[name[0]]))
+		temp = temp*count
+		Feature_labels.append(temp)
+		# i=i+h
+	import pdb; pdb.set_trace()
+	print (Feature_labels)
+
+	# n, labels = Clustering(Feature_labels)
+	# print("no. of lables:", n)
+	# print("*", labels)
 
 
 if __name__=="__main__":
