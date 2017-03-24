096f36a4c22eabe27a8eb4972e9007402bd019c0
Chandrakant Ojha <chandrkant.o14@iiits.in>
2017-03-24 23:05:17 +0530

    add

--- training.py
+++ training.py
@@ -12,13 +12,14 @@ import os
 # cap = cv2.VideoCapture("videos/person15_running_d1_uncomp.avi")
 labels = {'walking':1, 'Jogging':2, 'Handclapping': 3, 'Handwaving': 4, 'running': 5, 'boxing': 6 }
 global Frames
-	# params for ShiTomasi corner detection
+
+# params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
 	                   qualityLevel = 0.3,
 	                   minDistance = 7,
 	                   blockSize = 7 )
 
-	# Parameters for lucas kanade optical flow
+# Parameters for lucas kanade optical flow
 lk_params = dict( winSize  = (15,15),
 	              maxLevel = 2,
 	              criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
@@ -93,38 +94,6 @@ def ExtractFeatures(cap):
 	cap.release()
 	return count
 
-
-# def Clustering(Frames):
-	
-# 	iter_=2
-# 	prev_var=0
-# 	while iter_ < len(Frames)/2:
-# 		kmeans = KMeans(n_clusters=iter_, random_state=0).fit(Frames)
-# 		new_var = 0
-# 		for j in range(iter_):
-# 			indices = [i for i, x in enumerate(kmeans.labels_) if x == j]
-# 			li=list()
-# 			for k in range(len(indices)):
-# 				li.append(Frames[k])
-# 			li = np.asarray(li)
-# 			new_var+=np.var(li)
-		
-# 		new_var = math.sqrt(new_var)
-# 		print(iter_)
-# 		print("new_var:", new_var)
-
-# 		if iter_==2:
-# 			prev_var = new_var
-# 		elif new_var < prev_var:
-# 			prev_var = new_var
-# 		else:
-# 			break
-			
-# 		iter_+=1
-# 		prev_kmeansLabels = kmeans.labels_
-
-# 	return iter_, prev_kmeansLabels
-
 def main():
 	# labels = list()
 	global labels
@@ -147,13 +116,7 @@ def main():
 		temp.append(int(labels[name[0]]))
 		temp = temp*count
 		Feature_labels.append(temp)
-		# i=i+h
-	import pdb; pdb.set_trace()
-	print (Feature_labels)
-
-	# n, labels = Clustering(Feature_labels)
-	# print("no. of lables:", n)
-	# print("*", labels)
+	print ("Feature_labels: ", Feature_labels)
 
 
 if __name__=="__main__":
